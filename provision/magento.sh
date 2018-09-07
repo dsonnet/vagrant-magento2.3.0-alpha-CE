be1686682de3a5e6647655271f1f08971e834fb6
Benjamin Calef <benjamin.calef@caudalie.com>
2018-09-07 18:02:19 +0200

    fix package name

--- provision/magento.sh
+++ provision/magento.sh
@@ -153,7 +153,7 @@ apt-get update -y
 # Install packages
 apt-get install -y php7.2-mysql php7.2-cli php7.2-curl php7.2-mbstring php7.2-xml php7.2-mysql \
   php7.2-json php7.2-cgi php7.2-gd php-imagick php7.2-bz2 php7.2-zip \
-  php7.2-gd php7.2-intl php7.2-xsl php7.2-soa php7.2-bcmath
+  php7.2-gd php7.2-intl php7.2-xsl php7.2-soap php7.2-bcmath
 
 # ---------------------------------------------------------------------------------------------------------------------
 echoTitle 'Setting: PHP with Apache'
